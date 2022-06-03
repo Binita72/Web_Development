4d28d228934d4547e223f09f218db923dc8f158e
Binita Kumari Mandal <binitamandal74@gmail.com>
2022-06-03 22:28:41 +0530

    def_class

--- myfile.py
+++ myfile.py
@@ -1,1 +1,12 @@
-print("WORKING!")
+class Student():
+
+    def __init__(self, name, gpa):
+
+        self.name = name
+        self.gpa = gpa
+
+
+stud1 = Student(name='Binita', gpa=4.5)
+stud2 = Student(name='Moureen', gpa=4.8)
+
+print(stud2.gpa)
