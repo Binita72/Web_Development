f112c0044279ec4165a122342b91a79284cfe0d7
Binita Kumari Mandal <binitamandal74@gmail.com>
2022-06-08 22:27:00 +0530

    modules

---
+++ mymodule.py
@@ -0,0 +1,11 @@
+def useful_fun():
+    print("Using the useful func!")
+
+
+class UsefulClass():
+
+    def __init__(self, message):
+        self.message = message
+
+    def report(self):
+        print(self.message)
---
+++ myprogram.py
@@ -0,0 +1,3 @@
+from mymodule import useful_fun
+
+useful_fun()
